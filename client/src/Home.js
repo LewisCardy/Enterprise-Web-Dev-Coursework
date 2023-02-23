ae4b7ce0b7abca415677ff88b0c872163f53d0db
LewisCardy <91138837+LewisCardy@users.noreply.github.com>
2023-02-23 16:13:04 +0000

    Displaying the calculated quote from the server.

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,38 +1,52 @@
 import React, {useState, setState} from "react";
 import Axios from "axios";
 
+//the home component and main page for the website to calculate quotes
 export const Home = () => {
-    const [employeeList, setEmployeeList] = useState([{employeeType: "", employeeHours: ""}]);
-    const EmployeeAdd = () => {
+    const [employeeList, setEmployeeList] = useState([{employeeType: "", employeeHours: ""}]); //list of employees to add multiple instead of one.
+    const EmployeeAdd = () => { //adds employee input boxes to the webpage
         setEmployeeList([...employeeList, {employeeType: "", employeeHours: ""}]);
     }
 
-    const EmployeeRemove = (index) => {
+    const EmployeeRemove = (index) => { //handles removing an employee
         const newEmployeeList = [...employeeList];
         newEmployeeList.splice(index, 1);
         setEmployeeList(newEmployeeList);
     }
 
-    const [itemList, setItemList] = useState([{itemName: "", itemPrice: ""}]);
+    const [itemList, setItemList] = useState([{itemName: "", itemPrice: ""}]); //list of items to add to the dom.
 
-    const ItemAdd = () => {
+    const ItemAdd = () => { //adds item to the html
         setItemList([...itemList, {itemName: "", itemPrice: ""}]);
     }
 
-    const ItemRemove = (index) => {
+    const ItemRemove = (index) => { //removes item from html
         const newItemList = [...itemList];
         newItemList.splice(index, 1);
         setItemList(newItemList);
     }
 
+    const handleEmployeeChange = (e, index) => { //when the input for the employee changes
+        const {name, value} = e.target
+        const newEmployeeList = [...employeeList];
+        newEmployeeList[index][name] = value;
+        setEmployeeList(newEmployeeList)
+    }
+    const handleItemChange = (e, index) => { //input for item change
+        const {name, value} = e.target
+        const newItemList = [...itemList];
+        newItemList[index][name] = value;
+        setItemList(newItemList)
+    }
+
     const url = "/getQuote"
-    const [quote, setQuote] = useState({
+    const [quote, setQuote] = useState({ //structure of the quote to be updated and sent to the backend
         projectName: "",
         projectDescription: "",
         employees: "",
         items: ""
     })
-    function handleQuoteChange(e){
+    function handleQuoteChange(e){ //when the input is changed update the quote data
         const newQuote={...quote}
         newQuote[e.target.id] = e.target.value
         newQuote.employees = employeeList
@@ -41,7 +55,7 @@ export const Home = () => {
         
     }
 
-    function sendQuote(e){
+    function sendQuote(e){ //sends the quote to the url in the server which will handle the data, process and calcualte the final quote
         e.preventDefault()
         Axios.post(url,{
             projectName: quote.projectName,
@@ -53,27 +67,15 @@ export const Home = () => {
         })
     }
 
+    const [finalQuote, setFinalQuote] = useState({text: ""}) //used for getting the quote back from the server
     function getQuote(){
-        Axios.get("/sendQuote").then( res =>{
-            const quote = res.data;
-            this.setState({text: quote})
+        Axios.get("/sendQuote").then( res =>{ //gets the quote calculation from the server and updates the text
+            const newFinalQuote = res.data;
+            setFinalQuote({text: newFinalQuote})
         });
-        return quote;
     }
-
-
-    const handleEmployeeChange = (e, index) => {
-        const {name, value} = e.target
-        const newEmployeeList = [...employeeList];
-        newEmployeeList[index][name] = value;
-        setEmployeeList(newEmployeeList)
-    }
-    const handleItemChange = (e, index) => {
-        const {name, value} = e.target
-        const newItemList = [...itemList];
-        newItemList[index][name] = value;
-        setItemList(newItemList)
-    }
+    
+    
     // console.log(quote)
     return (
         <div>
@@ -82,6 +84,7 @@ export const Home = () => {
             <h1 class="text-2xl mb-5 p-5">Generate a quote for your project</h1>
                 <div class="py-2 px-6 shadow rounded-lg sm:px-10">
                     <form class="mb-0 space-y-6">
+                        {/* Project Details */}
                         <div>
                             <h2>Project Details</h2>
                             <div>
@@ -97,6 +100,7 @@ export const Home = () => {
                                 </div>
                             </div>
                         </div>
+                        {/* Employyes */}
                         <div>
                             <h2>Employees</h2>
                             {employeeList.map((singleEmployee, index) => (
@@ -130,6 +134,7 @@ export const Home = () => {
                                 <button class="hover:text-orange-500 hover:font-semibold text-sm text-blue-400" type="button" onClick={EmployeeAdd}>Add Employee</button>
                             </div>
                         </div>
+                        {/* Extra Items */}
                         <div>
                             <h2>Extras</h2>
                             {itemList.map((singleItem, index) => (
@@ -157,16 +162,18 @@ export const Home = () => {
                                 <button class="hover:text-orange-500 hover:font-semibold text-sm text-blue-400" type="button" onClick={ItemAdd}>Add Item</button>
                             </div>
                         </div>
+                        {/* Get quote button */}
                         <div class="grid grid-cols-1 justify-center">
                             <button class="bg-orange-400 rounded-lg shadow-lg m-2 p-2 hover:bg-orange-500 hover:font-semibold" type="button" onClick={(e)=> {sendQuote(e); getQuote()}}>Get Quote</button>
                         </div>
+                        {/* Display final quote */}
                         <div class="grid grid-cols-2 items-center justify-center">
                             <h3 class="">Your Final Quote Is...</h3>
-                            <h1 class=" text-4xl underline text-orange-400">{}</h1>
+                            <h1 class=" text-4xl underline text-orange-400">{finalQuote.text}</h1>
                         </div>
                         
                     </form>
-                    <div class="flex justify-end m-0">
+                    <div class="flex justify-end mt-5">
                             <button class="bg-orange-400 rounded-lg shadow-lg p-2 hover:bg-orange-500 hover:font-semibol">Save Quote</button>
                     </div>
                 </div>
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routesHandler = require('./routes/handler.js')
 const cors = require('cors')
 
 
@@ -8,7 +7,69 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use('/', routesHandler)
+
+let finalCost = 0; //final quote cost
+
+app.use('/getQuote', (req, res) => { //gets the quote sent from the front end
+    let data = req.body;
+    CalculateProjectCost(data) //calculates quote
+})
+
+app.get('/sendQuote', (req, res) => { //sends the new calculated quote back to the front end
+    let quotePrice = finalCost;
+    res.end(JSON.stringify(quotePrice))
+    console.log(quotePrice)
+})
+
+function CalculateProjectCost(data){ //calculates the quote using the data sent from the front end
+    finalCost = CalculateEmployeeCost(data) + CalculateItemCost(data)
+    let projectName = data.projectName;
+    let projectDescription = data.projectDescription;
+    
+}
+
+function CalculateEmployeeCost(data){ //calculates the pay for the employees
+    //standard hourly rates depending on employee type
+    let juniorPay = 10;
+    let standardPay = 20;
+    let seniorPay = 30;
+
+    //base hours
+    let juniorHours = 0;
+    let standardHours = 0;
+    let seniorHours = 0;
+
+    let employees = data.employees; //employees array from the full data
+
+    employees.forEach(employee => { //for each employee in the array calculate hours depening on what type they are
+        if(employee.employeeType == "Junior"){
+            juniorHours = juniorHours + parseInt(employee.employeeHours);
+        } else if (employee.employeeType == "Standard"){
+            standardHours = standardHours + parseInt(employee.employeeHours) ;
+        } else if (employee.employeeType == "Senior"){
+            seniorHours = seniorHours + parseInt(employee.employeeHours) ;
+        }
+    });
+    
+    //calulates the final pay depening on the hours and the base rates
+    let totalJuniorPay = juniorHours * juniorPay;
+    let totalStandardPay = standardHours * standardPay;
+    let totalSeniorPay = seniorHours * seniorPay;
+    
+    let totalPay = totalJuniorPay + totalStandardPay + totalSeniorPay; //total pay for employees
+    return totalPay;
+}
+function CalculateItemCost(data){ //calculates the cost of the extra items
+    let itemPrice = 0;
+    let items = data.items;
+
+    items.forEach(item => { //for each item run a total of the cost
+        itemPrice = itemPrice + parseInt(item.itemPrice) 
+    });
+    return itemPrice;
+}
+
+
 
 app.listen(5000, () => {
     console.log("Server started on port 5000")
