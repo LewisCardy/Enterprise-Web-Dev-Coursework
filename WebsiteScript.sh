3875134f187a0828a488392b21d1110480b0d96c
LewisCardy <91138837+LewisCardy@users.noreply.github.com>
2023-04-07 15:45:24 +0100

    Added script

---
+++ WebsiteScript.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+useradd -m user1 
+passwd user1
+usermod -aG sudo user1
+apt update
+apt install sudo
+sudo apt-get install -y curl
+curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh
+sudo apt-get install -y nodejs
+sudo apt-get install wget
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo apt-get install -y git 
+git clone https://github.com/LewisCardy/Enterprise-Web-Dev-Coursework.git
+cd Enterprise-Web-Dev-Coursework/
+cd client
+npm install
+cd ..
+cd server
+npm install
+cd
+mkdir database
