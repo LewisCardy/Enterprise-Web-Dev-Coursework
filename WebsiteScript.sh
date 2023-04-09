b1e2f13d6e7abbe975121f4411d725bf8f4ab22c
LewisCardy <91138837+LewisCardy@users.noreply.github.com>
2023-04-09 17:53:57 +0100

    Update WebsiteScript.sh

--- WebsiteScript.sh
+++ WebsiteScript.sh
@@ -9,6 +9,7 @@ curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh
 sudo apt-get install -y nodejs
 sudo apt-get install wget
+sudo apt-get install -y gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
